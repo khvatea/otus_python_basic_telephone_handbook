654b4af2b6846775b5463c45a01f854691610dfa
Evgeniy Khvat <evgeniy.khvat@gmail.com>
2024-12-04 22:43:35 +0300

    Initial breakdown of the application into classes

---
+++ main.py
@@ -0,0 +1,322 @@
+import json
+import sys
+import os
+
+from prettytable import PrettyTable
+import difflib
+
+class Database:
+    def __init__(self, file: str):
+        """
+         Init and Open phone book from file.
+         Writes a List object with contacts in dict format.
+        :param file: JSON file containing phone book contact entries
+        """
+        self.file = file
+        self.file_buff = "buff.{}".format(file)
+        self.contacts = []
+
+    def open(self):
+        # Open file, read and pass to list object
+        with open(self.file, "r", encoding='utf8') as stream:
+            handbook_json = json.load(stream)
+
+        # Create a temporary buffer file
+        with open(self.file_buff, "w", encoding='utf8') as stream:
+            json.dump(handbook_json, stream, ensure_ascii=False, indent=4)
+
+        self.contacts = handbook_json
+
+    def save(self):
+        """
+        Save the handbook in JSON format
+        """
+        with open(self.file, "w", encoding='utf8') as stream:
+            json.dump(self.contacts, stream, ensure_ascii=False, indent=4)
+
+    def compare(self) -> list:
+        """
+        Comparison of the handbook before and after the change
+        :return: List of changes
+        """
+        # Read the original handbook file
+        with open(self.file, "r", encoding='utf8') as origin:
+            source_json = json.load(origin)
+
+        # Saving intermediate changes to a handbook buffer
+        with open(self.file_buff, "w", encoding='utf8') as buffer:
+            json.dump(self.contacts, buffer, ensure_ascii=False, indent=4)
+        # Reread the stream from the beginning of the file
+        with open(self.file_buff, "r", encoding='utf8') as buffer:
+            source_buffer = json.load(buffer)
+
+        ret = []
+        before = [str(row) for row in source_json]
+        after = [str(row) for row in source_buffer]
+        difflist = difflib.ndiff(before, after)
+
+        for line in difflist:
+            if line.startswith(u'+'):
+                ret.append("\033[3m\033[32m{}\033[0m\n".format(line))
+            elif line.startswith(u'-'):
+                ret.append("\033[3m\033[31m{}\033[0m\n".format(line))
+            elif line.startswith(u'?'):
+                ret.append("{}".format(line))
+        return ret
+
+    def close(self):
+        if os.path.exists(self.file_buff):
+            os.remove(self.file_buff)
+        print("Выход...")
+
+
+
+class Handbook(Database):
+
+    @staticmethod
+    def show_pretty_table(sort_by_field="name", contacts=[]) -> str:
+        """
+        Get all handbook entries sorted by the selected field (by default, sorting will be done by the 'name' field)
+        The list should be presented as follows:
+        [
+            {
+                "name": "Ivan Ivanov",
+                "phone": "+7 111 111-11-11",
+                "email": "ivan.ivanov@example.com",
+                "address": "Moscow, st. Lenina, 1"
+            }
+        ]
+        :param sort_by_field: Table sort field
+        :param contacts: Contact list
+        :return: Return string representation of table in current state
+        """
+        pretty_table = PrettyTable()
+
+        # set table field names
+        pretty_table.field_names = ["name", "phone", "email", "address"]
+        pretty_table.align["name"] = "l"
+        pretty_table.align["address"] = "l"
+
+        # place all lines from the dictionary into a table
+        for contact in contacts:
+            pretty_table.add_row(list(contact.values()))
+
+        return pretty_table.get_string(sortby=sort_by_field)
+
+
+    def add_row(self, new_contact: dict):
+        """
+        Add an entry to the handbook
+        :param new_contact: New contact in dict format
+        :return:
+        """
+        expected_field_names = {"name", "phone", "email", "address"}
+
+        if isinstance(new_contact, dict):
+            missing_keys = expected_field_names - new_contact.keys()
+            extra_keys = new_contact.keys() - expected_field_names
+
+            if missing_keys:
+                print(f"Ошибка при записи контакта. Отсутствуют ключи: {missing_keys}")
+            elif extra_keys:
+                print(f"Ошибка при записи контакта. Лишние ключи: {extra_keys}")
+            else:
+                self.contacts.append(new_contact)
+        else:
+            print(f"""
+            В функции передан: {type(new_contact)}
+            Требуется dict
+            """)
+
+
+    def find_rows(self, search_string: str, find_by_field="name") -> list:
+        """
+        Searching for contacts in the handbook
+        :param search_string: Search string
+        :param find_by_field: Field to search for
+        :return: Search result
+        """
+        result = [contact for contact in self.contacts if search_string in contact[find_by_field]]
+        return result
+
+
+    def update_row(self, name: str, update_contact: dict) -> dict:
+        """
+        Update a handbook entry
+        :param name: Name of the record that is being updated
+        :param update_contact: Data according to handbook fields in dict format
+        :return: Updated contact
+        """
+        for contact in self.contacts:
+            if contact["name"] == name:
+                contact.update(update_contact)
+                return contact
+
+
+    def delete_row(self, name: str):
+        """
+        Removing an entry from the handbook
+        :param name: Name of the entry to be deleted
+        :return: Remove contact in dict format
+        """
+        for contact in self.contacts:
+            if contact["name"] == name:
+                self.contacts.remove(contact)
+                return contact
+        return None
+
+
+
+class Menu:
+
+    def __init__(self, handbook: Handbook):
+        self.handbook = handbook
+
+
+    def show(self, point: int) -> dict:
+        """
+        Main menu of the handbook
+        :param point: Menu item number
+        :return:
+        """
+        start_points = {1: ("Открыть справочник",    "item_open_handbook()"),
+                        2: ("Выйти",                 "item_close_handbook()")}
+
+        full_points = {1: ("Показать все контакты",  "item_print_handbook()"),
+                       2: ("Показать изменения",     "item_diff_handbook()"),
+                       3: ("Создать контакт",        "item_add_row()"),
+                       4: ("Найти контакт",          "item_find_rows()"),
+                       5: ("Обновить контакт",       "item_update_row()"),
+                       6: ("Удалить контакт",        "item_delete_row()"),
+                       7: ("Сохранить справочник",   "item_save_handbook()"),
+                       8: ("Выйти",                  "item_close_handbook()")}
+
+        filter_by_field_points = {1: ("Сортировать по имени",     "item_sorted_handbook('name')"),
+                                  2: ("Сортировать по адресу",    "item_sorted_handbook('address')"),
+                                  3: ("Сортировать по телефон",   "item_sorted_handbook('phone')")}
+
+        if point == 2:
+            return filter_by_field_points
+        else:
+            if not self.handbook.contacts:
+                return start_points
+            else:
+                return full_points
+
+
+    def item_open_handbook(self):
+        self.handbook.open()
+        return 0
+
+
+    def item_print_handbook(self):
+        return 2
+
+
+    def item_sorted_handbook(self, sorted_by_field: str):
+        print(self.handbook.show_pretty_table(sorted_by_field, self.handbook.contacts))
+        return 0
+
+
+    def item_diff_handbook(self):
+        contacts_diff = self.handbook.compare()
+
+        if not contacts_diff:
+            sys.stdout.writelines("\033[34mИзменений в справочнике нет.\033[0m\n")
+        else:
+            sys.stdout.writelines(contacts_diff)
+
+        return 0
+
+
+    def item_add_row(self):
+        new_contact = {"name": "undefined", "phone": "undefined", "email": "undefined", "address": "undefined"}
+
+        print("\nЗаполните поля нового контакта")
+        for key, value in new_contact.items():
+            new_contact[key] = input(f"{key}: ")
+        self.handbook.add_row(new_contact)
+
+        return 0
+
+
+    def item_save_handbook(self):
+        contacts_diff = self.handbook.compare()
+
+        if not contacts_diff:
+            sys.stdout.writelines("\033[34mИзменений в справочнике нет.\033[0m\n")
+        else:
+            print("\nОбнаружены следующие изменения:")
+            sys.stdout.writelines(contacts_diff)
+            if input("Сохранить изменения? (y/N)") == "y":
+                self.handbook.save()
+
+        return 0
+
+
+    def item_find_rows(self):
+        find_contacts = self.handbook.find_rows(input("\nКого найти?: "))
+        print(self.handbook.show_pretty_table(contacts=find_contacts))
+
+        return 0
+
+    def item_update_row(self):
+        contact = input("\nВведите полное имя контакта: ")
+        find_contacts = self.handbook.find_rows(contact)
+
+        if find_contacts:
+            update_contact = find_contacts[0]
+            print(f"\nОбновите поля контакта {update_contact['name']}")
+            for key, value in update_contact.items():
+                item = input(f"{key} (сейчас ==> {value}): ")
+                if item != "":
+                    update_contact[key] = item
+            print(self.handbook.update_row(contact, update_contact))
+        else:
+            sys.stdout.writelines("\033[34mЗапрошенный контакт не найден.\033[0m\n")
+
+        return 0
+
+
+    def item_delete_row(self):
+        contact = input("\nВведите полное имя контакта для удаления: ")
+
+        register = self.handbook.delete_row(contact)
+        if register:
+            print(f"Запись {register} успешно удалена.")
+        else:
+            print("""
+                        Контакт не удален, т.к. не был найден.
+                        Убедитесь в правильности и полноте указанного имени.
+                        Воспользуйтесь поиском по контактам.
+                        """)
+        return 0
+
+
+    def item_close_handbook(self):
+        self.handbook.close()
+        exit(0)
+
+
+
+####################################################e
+def main():
+    handbook = Handbook("handbook.json")
+    menu = Menu(handbook)
+    menu_return_point = 0
+
+    while True:
+        print("\n\n##############################")
+        menu_items = menu.show(menu_return_point)
+
+        # Show menu
+        for key, value in menu_items.items():
+            print(f"{key}. {value[0]}")
+
+        # Select action
+        menu_user_choice = input("Введите номер действия: ")
+        menu_return_point = eval(f"menu.{menu_items[int(menu_user_choice)][1]}")
+
+
+if __name__ == "__main__":
+    main()
